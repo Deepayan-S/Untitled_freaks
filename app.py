0d64db2b6f9db79b1cc87e997541f643708eede3
Deepayan <deepayansrichandan0@gmail.com>
2024-09-13 00:56:40 +0530

    Database

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask , render_template
-app=Flask(__name__)
+from flask import Flask , render_template ,url_for, request,redirect
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/')
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataBase.db'
+db = SQLAlchemy(app)
+
+class userInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userFirstName = db.Column(db.String(1000), nullable=False)
+    userLastName = db.Column(db.String(1000), nullable=False)
+    userPassword = db.Column(db.String(1000), nullable=False)
+    userEmail = db.Column(db.String(1000), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+@app.route('/',methods =['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method=='POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        UserInfo = userInfo(firstName=firstName,lastName=lastName,email=email,password=password)
+        try:   
+            db.session.add(UserInfo)
+            db.session.commit()
+        except:
+            return 'Error while logging'
+    else:
+        return render_template('login.html')
 
 @app.route('/joblisting/')
 def joblisting():
@@ -12,3 +39,8 @@ def joblisting():
 @app.route('/profile/')
 def profile():
     return render_template('profile.html')
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
